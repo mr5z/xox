b3a4fb48205b0cfd0a182ef637b3165685e12949
mark <dev.kram5@gmail.com>
2019-09-08 14:32:39 +0800

    Added files

---
+++ XOX/Form1.Designer.vb
@@ -0,0 +1,284 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.btn1 = New System.Windows.Forms.Button()
+        Me.gbButtons = New System.Windows.Forms.GroupBox()
+        Me.btn9 = New System.Windows.Forms.Button()
+        Me.btn8 = New System.Windows.Forms.Button()
+        Me.btn7 = New System.Windows.Forms.Button()
+        Me.btn6 = New System.Windows.Forms.Button()
+        Me.btn5 = New System.Windows.Forms.Button()
+        Me.btn4 = New System.Windows.Forms.Button()
+        Me.btn3 = New System.Windows.Forms.Button()
+        Me.btn2 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.lblScore = New System.Windows.Forms.Label()
+        Me.btnReset = New System.Windows.Forms.Button()
+        Me.lblNotify = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.gbButtons.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'btn1
+        '
+        Me.btn1.BackColor = System.Drawing.SystemColors.Control
+        Me.btn1.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btn1.FlatAppearance.BorderSize = 4
+        Me.btn1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(153, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.btn1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btn1.Font = New System.Drawing.Font("Britannic Bold", 36.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn1.Location = New System.Drawing.Point(6, 12)
+        Me.btn1.Name = "btn1"
+        Me.btn1.Size = New System.Drawing.Size(92, 92)
+        Me.btn1.TabIndex = 0
+        Me.btn1.Text = "X"
+        Me.btn1.UseVisualStyleBackColor = False
+        '
+        'gbButtons
+        '
+        Me.gbButtons.Controls.Add(Me.btn9)
+        Me.gbButtons.Controls.Add(Me.btn8)
+        Me.gbButtons.Controls.Add(Me.btn7)
+        Me.gbButtons.Controls.Add(Me.btn6)
+        Me.gbButtons.Controls.Add(Me.btn5)
+        Me.gbButtons.Controls.Add(Me.btn4)
+        Me.gbButtons.Controls.Add(Me.btn3)
+        Me.gbButtons.Controls.Add(Me.btn2)
+        Me.gbButtons.Controls.Add(Me.btn1)
+        Me.gbButtons.Location = New System.Drawing.Point(12, 62)
+        Me.gbButtons.Name = "gbButtons"
+        Me.gbButtons.Size = New System.Drawing.Size(302, 306)
+        Me.gbButtons.TabIndex = 1
+        Me.gbButtons.TabStop = False
+        '
+        'btn9
+        '
+        Me.btn9.BackColor = System.Drawing.SystemColors.Control
+        Me.btn9.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btn9.FlatAppearance.BorderSize = 4
+        Me.btn9.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(153, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.btn9.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btn9.Font = New System.Drawing.Font("Britannic Bold", 36.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn9.Location = New System.Drawing.Point(202, 208)
+        Me.btn9.Name = "btn9"
+        Me.btn9.Size = New System.Drawing.Size(92, 92)
+        Me.btn9.TabIndex = 9
+        Me.btn9.Text = "X"
+        Me.btn9.UseVisualStyleBackColor = False
+        '
+        'btn8
+        '
+        Me.btn8.BackColor = System.Drawing.SystemColors.Control
+        Me.btn8.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btn8.FlatAppearance.BorderSize = 4
+        Me.btn8.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(153, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.btn8.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btn8.Font = New System.Drawing.Font("Britannic Bold", 36.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn8.Location = New System.Drawing.Point(104, 208)
+        Me.btn8.Name = "btn8"
+        Me.btn8.Size = New System.Drawing.Size(92, 92)
+        Me.btn8.TabIndex = 8
+        Me.btn8.Text = "X"
+        Me.btn8.UseVisualStyleBackColor = False
+        '
+        'btn7
+        '
+        Me.btn7.BackColor = System.Drawing.SystemColors.Control
+        Me.btn7.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btn7.FlatAppearance.BorderSize = 4
+        Me.btn7.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(153, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.btn7.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btn7.Font = New System.Drawing.Font("Britannic Bold", 36.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn7.Location = New System.Drawing.Point(6, 208)
+        Me.btn7.Name = "btn7"
+        Me.btn7.Size = New System.Drawing.Size(92, 92)
+        Me.btn7.TabIndex = 7
+        Me.btn7.Text = "X"
+        Me.btn7.UseVisualStyleBackColor = False
+        '
+        'btn6
+        '
+        Me.btn6.BackColor = System.Drawing.SystemColors.Control
+        Me.btn6.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btn6.FlatAppearance.BorderSize = 4
+        Me.btn6.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(153, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.btn6.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btn6.Font = New System.Drawing.Font("Britannic Bold", 36.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn6.Location = New System.Drawing.Point(202, 110)
+        Me.btn6.Name = "btn6"
+        Me.btn6.Size = New System.Drawing.Size(92, 92)
+        Me.btn6.TabIndex = 6
+        Me.btn6.Text = "X"
+        Me.btn6.UseVisualStyleBackColor = False
+        '
+        'btn5
+        '
+        Me.btn5.BackColor = System.Drawing.SystemColors.Control
+        Me.btn5.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btn5.FlatAppearance.BorderSize = 4
+        Me.btn5.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(153, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.btn5.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btn5.Font = New System.Drawing.Font("Britannic Bold", 36.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn5.Location = New System.Drawing.Point(104, 110)
+        Me.btn5.Name = "btn5"
+        Me.btn5.Size = New System.Drawing.Size(92, 92)
+        Me.btn5.TabIndex = 5
+        Me.btn5.Text = "X"
+        Me.btn5.UseVisualStyleBackColor = False
+        '
+        'btn4
+        '
+        Me.btn4.BackColor = System.Drawing.SystemColors.Control
+        Me.btn4.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btn4.FlatAppearance.BorderSize = 4
+        Me.btn4.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(153, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.btn4.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btn4.Font = New System.Drawing.Font("Britannic Bold", 36.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn4.Location = New System.Drawing.Point(6, 110)
+        Me.btn4.Name = "btn4"
+        Me.btn4.Size = New System.Drawing.Size(92, 92)
+        Me.btn4.TabIndex = 4
+        Me.btn4.Text = "X"
+        Me.btn4.UseVisualStyleBackColor = False
+        '
+        'btn3
+        '
+        Me.btn3.BackColor = System.Drawing.SystemColors.Control
+        Me.btn3.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btn3.FlatAppearance.BorderSize = 4
+        Me.btn3.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(153, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.btn3.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btn3.Font = New System.Drawing.Font("Britannic Bold", 36.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn3.Location = New System.Drawing.Point(202, 12)
+        Me.btn3.Name = "btn3"
+        Me.btn3.Size = New System.Drawing.Size(92, 92)
+        Me.btn3.TabIndex = 3
+        Me.btn3.Text = "X"
+        Me.btn3.UseVisualStyleBackColor = False
+        '
+        'btn2
+        '
+        Me.btn2.BackColor = System.Drawing.SystemColors.Control
+        Me.btn2.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btn2.FlatAppearance.BorderSize = 4
+        Me.btn2.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(153, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.btn2.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btn2.Font = New System.Drawing.Font("Britannic Bold", 36.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn2.Location = New System.Drawing.Point(104, 12)
+        Me.btn2.Name = "btn2"
+        Me.btn2.Size = New System.Drawing.Size(92, 92)
+        Me.btn2.TabIndex = 1
+        Me.btn2.Text = "X"
+        Me.btn2.UseVisualStyleBackColor = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(15, 15)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(93, 31)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Score:"
+        '
+        'lblScore
+        '
+        Me.lblScore.AutoSize = True
+        Me.lblScore.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblScore.ForeColor = System.Drawing.SystemColors.Control
+        Me.lblScore.Location = New System.Drawing.Point(114, 16)
+        Me.lblScore.Name = "lblScore"
+        Me.lblScore.Size = New System.Drawing.Size(66, 31)
+        Me.lblScore.TabIndex = 4
+        Me.lblScore.Text = "0 : 0"
+        '
+        'btnReset
+        '
+        Me.btnReset.BackColor = System.Drawing.SystemColors.ControlDarkDark
+        Me.btnReset.FlatAppearance.BorderSize = 2
+        Me.btnReset.FlatAppearance.MouseDownBackColor = System.Drawing.SystemColors.Control
+        Me.btnReset.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Gray
+        Me.btnReset.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnReset.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnReset.ForeColor = System.Drawing.SystemColors.Control
+        Me.btnReset.Location = New System.Drawing.Point(199, 12)
+        Me.btnReset.Name = "btnReset"
+        Me.btnReset.Size = New System.Drawing.Size(115, 44)
+        Me.btnReset.TabIndex = 5
+        Me.btnReset.Text = "Reset"
+        Me.btnReset.UseVisualStyleBackColor = False
+        '
+        'lblNotify
+        '
+        Me.lblNotify.AutoSize = True
+        Me.lblNotify.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblNotify.ForeColor = System.Drawing.SystemColors.Control
+        Me.lblNotify.Location = New System.Drawing.Point(15, 377)
+        Me.lblNotify.Name = "lblNotify"
+        Me.lblNotify.Size = New System.Drawing.Size(135, 26)
+        Me.lblNotify.TabIndex = 6
+        Me.lblNotify.Text = "Player 1 turn"
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 300
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ControlDarkDark
+        Me.ClientSize = New System.Drawing.Size(328, 417)
+        Me.Controls.Add(Me.lblNotify)
+        Me.Controls.Add(Me.btnReset)
+        Me.Controls.Add(Me.lblScore)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.gbButtons)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+        Me.Name = "frmMain"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "X.O.X"
+        Me.gbButtons.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btn1 As System.Windows.Forms.Button
+    Friend WithEvents gbButtons As System.Windows.Forms.GroupBox
+    Friend WithEvents btn3 As System.Windows.Forms.Button
+    Friend WithEvents btn2 As System.Windows.Forms.Button
+    Friend WithEvents btn9 As System.Windows.Forms.Button
+    Friend WithEvents btn8 As System.Windows.Forms.Button
+    Friend WithEvents btn7 As System.Windows.Forms.Button
+    Friend WithEvents btn6 As System.Windows.Forms.Button
+    Friend WithEvents btn5 As System.Windows.Forms.Button
+    Friend WithEvents btn4 As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents lblScore As System.Windows.Forms.Label
+    Friend WithEvents btnReset As System.Windows.Forms.Button
+    Friend WithEvents lblNotify As System.Windows.Forms.Label
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+
+End Class
---
+++ XOX/Form1.vb
@@ -0,0 +1,198 @@
+﻿Public Class frmMain
+
+    Dim toggle As Boolean
+    Dim scoreA, scoreB As Integer
+    Dim btnA, btnB, btnC As Button
+
+    Dim counter As Integer = 0
+    Dim colorToggle = False
+    Dim prevBackColor As Color
+    Dim winBackColor As New Color
+
+    Private Sub btnReset_Click(sender As Object, e As EventArgs) Handles btnReset.Click
+        ResetBoard()
+        ResetScore()
+        UpdateScore()
+    End Sub
+
+    Private Sub btnReset_Click1(sender As Object, e As EventArgs) Handles btnReset.MouseDown
+        btnReset.ForeColor = Color.Green
+    End Sub
+    Private Sub btnReset_Click2(sender As Object, e As EventArgs) Handles btnReset.MouseUp
+        btnReset.ForeColor = Color.White
+    End Sub
+
+    Private Sub btn1_Click(sender As Object, e As EventArgs) _
+        Handles btn1.Click, btn2.Click, btn3.Click, _
+                btn4.Click, btn5.Click, btn6.Click, _
+                btn7.Click, btn8.Click, btn9.Click
+
+        If sender.Text <> "" Then Return
+
+        If toggle Then sender.Text = "X" Else sender.Text = "O"
+
+        If HasHorizontalMatch() Then
+        ElseIf HasVerticalMatch() Then
+        ElseIf HasDiagonalMatch() Then
+        Else
+            If IsDraw() Then
+                MsgBox("It's a draw!")
+                ResetBoard()
+            End If
+        End If
+
+        toggle = Not toggle
+        NotifyWhosTurn()
+
+    End Sub
+
+    Function HasHorizontalMatch() As Boolean
+        If IsSame(btn1, btn2, btn3) Then
+            EndMatch(btn1, btn2, btn3)
+            Return True
+        End If
+        If IsSame(btn4, btn5, btn6) Then
+            EndMatch(btn4, btn5, btn6)
+            Return True
+        End If
+        If IsSame(btn7, btn8, btn9) Then
+            EndMatch(btn7, btn8, btn9)
+            Return True
+        End If
+        Return False
+    End Function
+
+    Function HasVerticalMatch() As Boolean
+        If IsSame(btn1, btn4, btn7) Then
+            EndMatch(btn1, btn4, btn7)
+            Return True
+        End If
+        If IsSame(btn2, btn5, btn8) Then
+            EndMatch(btn2, btn5, btn8)
+            Return True
+        End If
+        If IsSame(btn3, btn6, btn9) Then
+            EndMatch(btn3, btn6, btn9)
+            Return True
+        End If
+        Return False
+    End Function
+
+    Function HasDiagonalMatch()
+        If IsSame(btn1, btn5, btn9) Then
+            EndMatch(btn1, btn5, btn9)
+            Return True
+        End If
+        Return False
+    End Function
+
+    Function IsDraw()
+        Return Not (Empty(btn1) Or Empty(btn2) Or Empty(btn3) _
+                 Or Empty(btn4) Or Empty(btn5) Or Empty(btn6) _
+                 Or Empty(btn7) Or Empty(btn8) Or Empty(btn9))
+    End Function
+
+    Function Empty(ByVal button As Button) As Boolean
+        Return button.Text = ""
+    End Function
+
+    Sub EndMatch(ByRef a As Button, ByRef b As Button, ByRef c As Button)
+
+        btnA = a
+        btnB = b
+        btnC = c
+
+        gbButtons.Enabled = False
+
+        Timer1.Enabled = True
+        Timer1.Start()
+    End Sub
+
+    Sub ResetScore()
+        scoreA = 0
+        scoreB = 0
+    End Sub
+
+    Sub WhoWin()
+        If toggle Then scoreB += 1 Else scoreA += 1
+    End Sub
+
+    Sub UpdateScore()
+        lblScore.Text = scoreA & " : " & scoreB
+    End Sub
+    Sub Notify(ByVal msg As String)
+        lblNotify.Text = msg
+    End Sub
+
+    Sub NotifyWhosTurn()
+        If toggle Then
+            Notify("Player 2 turn")
+        Else
+            Notify("Player 1 turn")
+        End If
+    End Sub
+
+    Function GetWinner() As String
+        If toggle Then Return "Player 2" Else Return "Player 1"
+    End Function
+
+    Sub ResetBoard()
+        Const DEF_VAL = ""
+        btn1.Text = DEF_VAL
+        btn2.Text = DEF_VAL
+        btn3.Text = DEF_VAL
+        btn4.Text = DEF_VAL
+        btn5.Text = DEF_VAL
+        btn6.Text = DEF_VAL
+        btn7.Text = DEF_VAL
+        btn8.Text = DEF_VAL
+        btn9.Text = DEF_VAL
+    End Sub
+
+    Function IsSame(ByVal a As Button, ByVal b As Button, ByVal c As Button)
+        If a.Text = "" Or b.Text = "" Or c.Text = "" Then Return False
+
+        Return a.Text = b.Text And a.Text = c.Text
+    End Function
+
+    Private Sub frmMain_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ResetBoard()
+        prevBackColor = btn1.BackColor
+        winBackColor = Color.DarkGreen
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+
+        If colorToggle Then
+            btnA.BackColor = prevBackColor
+            btnB.BackColor = prevBackColor
+            btnC.BackColor = prevBackColor
+        Else
+            btnA.BackColor = winBackColor
+            btnB.BackColor = winBackColor
+            btnC.BackColor = winBackColor
+        End If
+
+
+        colorToggle = Not colorToggle
+        counter += 1
+
+        If counter >= 4 Then
+            counter = 0
+            colorToggle = False
+
+            btnA.BackColor = prevBackColor
+            btnB.BackColor = prevBackColor
+            btnC.BackColor = prevBackColor
+
+            gbButtons.Enabled = True
+            Timer1.Enabled = False
+            Timer1.Stop()
+
+            WhoWin()
+            UpdateScore()
+            ResetBoard()
+        End If
+
+    End Sub
+End Class
